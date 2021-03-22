41e1a0f8374242ca8e47ddd6effb92d178a8d27d
Cristian <ncristiansalinasandia@gmail.com>
2021-03-22 15:41:40 +0100

    React router instalado y configurado

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+/* Comentamos esta línea de código ya que nuestra administración de rutas
+    se realizará con react-router
+*/
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+Route::view('/{path?}', 'app');
